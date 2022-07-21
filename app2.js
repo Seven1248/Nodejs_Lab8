5b2e97bcc41435ecc91d458fa51b64918f515fb3
Seven1248 <mayr81214@gmail.com>
2022-07-21 19:58:02 +0800

    Update app2.js
    今日測試

--- app2.js
+++ app2.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const multer = require('multer');
 const app = express();
+const storage = multer.memoryStorage();
 
 var upload = multer({
-    storage: myStorage,
+    storage: storage,   
     fileFilter: function(req, file , cd){
         if(file.mimetype != 'image/jpg'){
             return cd(new Error('Wrong file type'));
-        }
+        }cd(null, true);
     }});
 
 app.post('/upload_file', upload.single('myfile'), function(req, res){
     res.send('上傳成功' + '<input type ="button" onclick="history.back()" value="回到上一頁"></input>');
+    console.log(req.file, req.body);
 });
 app.get('/', function(req,res){
     res.sendFile(__dirname + '/index.html', function(err){
